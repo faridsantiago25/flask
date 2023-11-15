98e2dcf9458d46b32e4ef6db6f65060db7d27abf
unknown <faridsantiago1907@gmail.com>
2023-11-14 21:26:57 -0500

    trying

--- config/db.py
+++ config/db.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
+import os
 app = Flask(__name__)
 
 #creamos las credenciales para conectarnos a la bd
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456Farid25.@localhost/web_avanzada'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URL")
 app.config['SQLALCHEMY_TRACK_MODIFACATIONS'] = False
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = -1
 
